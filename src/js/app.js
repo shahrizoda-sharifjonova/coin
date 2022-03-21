5480b04d54c90700b1d747fe8845da88379d227e
shahrizoda <shahrizodadeveloping@gmail.com>
2022-03-21 23:09:59 +0500

    end menu block

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,9 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-// const swiper = new Swiper();
+const swiper = new Swiper();
 
 const dropdownTop = document.querySelector('.top__dropdown-top');
 const dropdownArrow = document.querySelector('.top__dropdown-arrow');
@@ -13,8 +13,17 @@ const dropdownItem = document.querySelectorAll('.top__dropdown-item');
 const dropdownImg = document.querySelectorAll('.top__dropdown-img img');
 
 dropdownTop.addEventListener('click', (e)=>{
-        dropdownContent.classList.toggle('active')
-        dropdownArrow.classList.toggle('active')
+  dropdownContent.classList.toggle('active')
+  dropdownArrow.classList.toggle('active')
 })
+
+new Swiper(".menu__swiper", {
+  slidesPerView: "auto",
+  modules: [Navigation],
+  spaceBetween: 25,
+  navigation: {
+    nextEl: ".menu__button-next",
+  },
+});
 
 
