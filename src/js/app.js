bb35aac2d184a1dc02ca54048678580105d7c2bb
shahrizoda <shahrizodadeveloping@gmail.com>
2022-03-21 20:48:37 +0500

    end top block

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+import * as functions from "./modules/functions.js";
+
+functions.isWebp();
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+// const swiper = new Swiper();
+
+const dropdownTop = document.querySelector('.top__dropdown-top');
+const dropdownArrow = document.querySelector('.top__dropdown-arrow');
+const dropdownContent = document.querySelector('.top__dropdown-content');
+const dropdownItem = document.querySelectorAll('.top__dropdown-item');
+const dropdownImg = document.querySelectorAll('.top__dropdown-img img');
+
+dropdownTop.addEventListener('click', (e)=>{
+        dropdownContent.classList.toggle('active')
+        dropdownArrow.classList.toggle('active')
+})
+
+
